fda30df74a2d58cced5389a6e45003a4a25201f2
Anton Tetov <anton@tetov.se>
2025-04-09 12:25:46 +0200

    launch migration, wip

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
-from glob import glob
+# from glob import glob
 import os
 from pathlib import Path
 
 from setuptools import setup
 
-package_name = 'biodigitalmatter_ros'
+package_name = "biodigitalmatter_ros"
 
-share_dir = Path('share') / package_name
+share_dir = Path("share") / package_name
 
 setup(
  name=package_name,
- version='0.0.0',
+ version="0.1.0",
  packages=[package_name],
  data_files=[
      ('share/ament_index/resource_index/packages',
              ['resource/' + package_name]),
      (str(share_dir), ['package.xml']),
-     (str(share_dir / "launch"), glob("*launch.[pxy][yma]*", root_dir="launch")),
-     (str(share_dir / "config"), glob("*.y?ml", root_dir="config"))
+     (str(share_dir / "launch"), [str(p) for p in Path("launch").glob("*launch.[pxy][yma]*")]),
+     (str(share_dir / "config"), [str(p) for p in Path("config").glob("*.y?ml")])
    ],
  install_requires=['setuptools'],
  zip_safe=True,
