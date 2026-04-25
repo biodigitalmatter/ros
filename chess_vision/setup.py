65d7521fb8516be5047872c53a3644d530a55fc8
Anton Tetov Johansson <anton@tetov.se>
2026-04-25 09:07:11 +0200

    refactor: s/test/tests

--- chess_vision/setup.py
+++ chess_vision/setup.py
@@ -6,7 +6,7 @@ package_name = "chess_vision"
 setup(
     name=package_name,
     version="0.1.0",
-    packages=find_packages(exclude=["test"]),
+    packages=find_packages(),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name + "/launch", glob.glob("launch/*")),
--- chess_vision/tests/chess_vision/test_charuco_tracker_node.py
+++ chess_vision/tests/chess_vision/test_charuco_tracker_node.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import sys
 from threading import Event, Thread
 
 import launch
@@ -28,21 +27,17 @@ def rosbag_path():
 
 @launch_pytest.fixture
 def generate_test_description(rosbag_path):
-    node_path = Path(__file__).parents[2] / "chess_vision" / "charuco_tracker_node.py"
-    print(f"{node_path=}")
     node = launch_ros.actions.Node(
-        executable=sys.executable,
-        arguments=[str(node_path), "--ros-args", "--log-level", "DEBUG"],
+        package="chess_vision",
+        executable="charuco_tracker_node",
         name="charuco_tracker",
         parameters=[{"board_name": "sample_calibration_video", "rectified": True}],
         remappings=[
             (
+                "image_raw",
                 "/topic_video",
-                "/image_raw",
             )
         ],
-        additional_env={"PYTHONUNBUFFERED": "1"},  # dont buffer stdout stderr
-        output="screen",
     )
     bag_play = launch.actions.ExecuteProcess(
         cmd=[
@@ -55,7 +50,6 @@ def generate_test_description(rosbag_path):
             "--loop",
             str(rosbag_path),
         ],
-        output="screen",
     )
     return launch.LaunchDescription(
         [node, bag_play, launch_pytest.actions.ReadyToTest()]
@@ -71,7 +65,7 @@ def test_check_if_msgs_published():
         node = MakeTestNode("test_node")
         node.start_subscriber()
         msgs_received_flag = node.msg_event_object.wait(timeout=5.0)
-        assert msgs_received_flag, "Did not receive msgs !"
+        assert msgs_received_flag, "Did not receive msgs!"
     finally:
         rclpy.shutdown()
 
