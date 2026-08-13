6542d4dd4bdfec64be6c8a31ff84171ba6e925cf
Anton Tetov Johansson <anton@tetov.se>
2026-08-13 16:56:13 +0200

    nixos: setup secrets for kaolin

--- nix/modules/networking.nix
+++ nix/modules/networking.nix
@@ -3,11 +3,14 @@
   self,
   ...
 }:
+let
+  sopsKey = "network/envfile";
+in
 {
   flake.modules.nixos = {
     core = {
-      # sops.secrets.${sopsKey} = { };
-      #
+      sops.secrets.${sopsKey} = { };
+
       networking = {
         useDHCP = true;
       };
@@ -24,19 +27,19 @@
     };
 
     "networking/wpa_supplicant" =
-      { ... }:
+      { config, ... }:
       {
         networking = {
           networkmanager.enable = lib.mkForce false;
-          # wireless = {
-          #   enable = true;
-          #   secretsFile = config.sops.secrets.${sopsKey}.path;
-          #   networks = {
-          #     GL-X3000-59f.pskRaw = "ext:GL_X3000_59f_psk";
-          #     Terra.pskRaw = "ext:Terra_psk";
-          #     # no eduroam defined in this format please
-          #   };
-          # };
+          wireless = {
+            enable = true;
+            secretsFile = config.sops.secrets.${sopsKey}.path;
+            networks = {
+              GL-X3000-59f.pskRaw = "ext:GL_X3000_59f_psk";
+              Terra.pskRaw = "ext:Terra_psk";
+              # no eduroam defined in this format please
+            };
+          };
         };
       };
   };
